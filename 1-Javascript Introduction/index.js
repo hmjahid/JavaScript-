89ba49e624450f8c4803343c57d7788dd0dc7d58
hmjahid <mdjahidhasan919@gmail.com>
2023-01-12 22:07:27 +0600

    first commit

---
+++ 1-Javascript Introduction/index.js
@@ -0,0 +1,16 @@
+console.log("I Love my family");
+console.log("I really love them");
+alert("My family is my heart");
+window.alert("My parents are my heaven");
+document.write("I am greatful to ALLAH for them");
+
+//This is a single line comment
+
+/*
+   This
+   is
+   a
+   multi
+   line
+   comments
+*/ 
