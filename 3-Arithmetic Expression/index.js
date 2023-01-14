837952191a4cf90edd2e885d10bcb48e7dff106e
hmjahid <mdjahidhasan919@gmail.com>
2023-01-14 12:03:52 +0600

    "JavaScript Arithmetic Expressions" practise

---
+++ 3-Arithmetic Expression/index.js
@@ -0,0 +1,51 @@
+let students = 25;
+console.log(students);
+
+ students = students + 1;
+console.log(students);
+
+students += 1;
+console.log(students);
+
+students = students - 1;
+console.log(students);
+
+students -= 1;
+console.log(students);
+
+students = students * 2;
+console.log(students);
+
+students *= 2;
+console.log(students);
+
+students = students / 2;
+console.log(students);
+
+students /= 2;
+console.log(students); 
+
+let extraStudents = students % 3;
+console.log(extraStudents);
+
+var students1 = 25;
+console.log(students1);
+
+var students1 = 50;
+console.log(students1);
+
+const students2 = 100;
+console.log(students2);
+
+let result = (2 * 8) / 4 - (45 / 9) * 3 + (6 + 9) * 7 - 21 * 5 - 10 + 12;
+
+console.log(result);
+
+let x;
+let y;
+
+x = 10;
+y = x + 11;
+
+console.log(y);
+
