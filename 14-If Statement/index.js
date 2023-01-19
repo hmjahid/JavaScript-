e00c5e318c266b8aa8db5d3e92f9dd206525b710
hmjahid <mdjahidhasan919@gmail.com>
2023-01-19 10:18:44 +0600

    "JavaScript If Statement" practise

---
+++ 14-If Statement/index.js
@@ -0,0 +1,100 @@
+let age = 26;
+
+if(age >= 18){
+    console.log("You are eligible to vote");
+
+}
+else{
+    console.log("You are not eligible to vote");
+
+}
+
+
+
+let age1 = 14;
+
+if(age1 >= 18){
+    console.log("You are eligible to vote");
+
+}
+else{
+    console.log("You are not eligible to vote");
+
+}
+
+
+
+let age2 = 65;
+
+if(age2 >= 65){
+    console.log("You are a senior Citizen");
+}
+else{
+    console.log("You are not a senior Citizen");
+}
+
+
+
+let age3 = 6;
+
+if(age3 >= 0){
+    console.log("You are a child");
+
+}
+else{
+    console.log("You haven't born yet");
+}
+
+
+
+
+let age4 = -8;
+
+if(age4 < 0){
+    console.log("You haven't born yet ");
+
+}
+else if(age4 >= 0){
+    console.log("You are a child");
+
+}
+else if(age4 >= 18){
+    console.log("You are eligible to vote");
+
+}
+else if(age4 = 21){
+    console.log("You are eligible to marry")
+}
+else if(age4 > 21){
+    console.log("You also more eligible to marry");
+
+}
+else if(age4 > 65){
+    console.log("You are a senior Citizen");
+
+}
+else{
+    console.log("You are not a senior Citizen");
+
+}
+
+
+let online = true;
+
+if(online){
+    console.log("You are online");
+}
+else{
+    console.log("You are offline");
+}
+
+
+
+online = false;
+
+if(online){
+    console.log("You are online");
+}
+else{
+    console.log("You are offline");
+}
