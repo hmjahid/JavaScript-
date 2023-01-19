9da62b49de13afbaa5212bbc0224eae9fdc50595
hmjahid <mdjahidhasan919@gmail.com>
2023-01-19 10:17:41 +0600

    "JavaScript Method Chaining" practise

---
+++ 13-Method Chaining/index.js
@@ -0,0 +1,15 @@
+let userName = "jahid";
+
+let firstLetter = userName.charAt(0);
+console.log(firstLetter);
+
+firstLetter = firstLetter.toUpperCase();
+console.log(firstLetter);
+
+firstLetter = firstLetter.trim();
+console.log(firstLetter);
+
+
+let secondLetter1 = userName.charAt(1).toUpperCase().trim();
+console.log(secondLetter1);
+
