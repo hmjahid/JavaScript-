3b4b6d67c2d4efd98c8412d13ab3018e918b4d64
hmjahid <mdjahidhasan919@gmail.com>
2023-01-20 18:51:45 +0600

    "JavaScript NOT Logical Operator" practise

---
+++ 18-NOT Logical Operaor/index.js
@@ -0,0 +1,51 @@
+let temp = 15;
+
+
+if (temp > 0){
+    console.log("It is warm outside");
+}
+else{
+    console.log("It is cold outside");
+}
+
+
+
+temp = 15;
+
+if (!(temp > 0)){
+    console.log("It is cold outside");
+}
+else{
+    console.log("It is warm outside");
+}
+
+
+temp = -15;
+
+if (!(temp > 0)){
+    console.log("It is warm outside");
+}
+else{
+    console.log("It is cold outside");
+}
+
+
+
+
+let sunny = true;
+
+
+if(sunny){
+    console.log("It is sunny outside");
+}
+else{
+    console.log("It is not cloudy outside");
+}
+
+ 
+if(!(sunny)){
+    console.log("It is cloudy outside");
+}
+else{
+    console.log("It is sunny outside");
+}
