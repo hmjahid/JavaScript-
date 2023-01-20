01cf7808aba84b0555fffc1c0800ef51f2498505
hmjahid <mdjahidhasan919@gmail.com>
2023-01-20 18:50:13 +0600

    "JavaScript AND OR Logical Operators" practise

---
+++ 17-AND OR Logical Operators/index.js
@@ -0,0 +1,48 @@
+let temp = 15;
+
+if (temp > 0 && temp < 30) {
+    console.log("The weather is good!");
+}
+else{
+    console.log("The weather is bad!");
+}
+
+
+
+
+
+temp = 50;
+
+if(temp <= 0 || temp >= 30){
+    console.log("The weather is bad!");
+}
+else{
+    console.log("The weather is good!");
+}
+
+
+
+
+temp = 15;
+let sunny = true;
+
+if (temp > 0 && temp < 30 && sunny == true) {
+    console.log("The weather is good!");
+}
+else{
+    console.log("The weather is bad!");
+}
+
+
+
+
+
+temp = 20;
+let sunlight = true;;
+
+if (temp > 0 && temp < 30 && sunlight) {
+    console.log("The weather is good!");
+}
+else{
+    console.log("The weather is bad!");
+}
