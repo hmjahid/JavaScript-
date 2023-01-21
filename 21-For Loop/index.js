47fd0c2796d71a6c54fe87d65def205300724964
hmjahid <mdjahidhasan919@gmail.com>
2023-01-21 17:47:42 +0600

    "JavaScript For Loop" practise

---
+++ 21-For Loop/index.js
@@ -0,0 +1,22 @@
+
+for(let i = 1; i <= 10; i += 1){
+    console.log(i);
+}
+
+
+
+
+for(let j = 10; j > 0; j -= 1){
+    console.log(j);
+    
+}
+
+
+
+
+for(let counter = 1; counter <= 10; counter +=1){
+    console.log(counter);
+}
+
+
+console.log("ASSALAMU ALAIKUM");
