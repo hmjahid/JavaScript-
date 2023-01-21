dd23d9388a73ae48bc0c921d8ddef48247013033
hmjahid <mdjahidhasan919@gmail.com>
2023-01-21 17:49:47 +0600

    "JavaScript Break and Continue Statement" practise

---
+++ 22-Break and Continue Statement/index.js
@@ -0,0 +1,31 @@
+
+for(let i = 1; i <= 20; i += 1){
+    if(i == 13){
+        break;
+    }
+    console.log(i);
+}
+
+
+
+
+for(let j = 20; j > 0; j -= 1){
+    if(j == 13){
+        continue;
+    }
+    console.log(j);
+    
+}
+
+
+
+
+for(let counter = 1; counter <= 30; counter +=1){
+    if(counter == 22){
+        break;
+    }
+    console.log(counter);
+}
+
+
+console.log("ASSALAMU ALAIKUM");
