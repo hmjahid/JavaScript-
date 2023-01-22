03495226bcfbf139c542be8b6e8be610cdfe901d
hmjahid <mdjahidhasan919@gmail.com>
2023-01-22 15:10:04 +0600

    "JavaScript Function" practise

---
+++ 24-Function/index.js
@@ -0,0 +1,17 @@
+
+userInfo();
+
+function userInfo(){
+    let userName = "Jahid";
+    let userAge = 28;
+
+    information(userName, userAge);
+}
+
+
+function information(userName, userAge){
+    console.log("Assalamu alaikum,", userName);
+    console.log("How are you?")
+    console.log("You are now", userAge, "years of old")
+    console.log("You are grown now");
+}
