d32fe703757801c0a141bba946b54aa36d32561c
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2023-02-21 11:49:33 +0600

    "JavaScript DOM" practise

---
+++ 71-DOM/index.js
@@ -0,0 +1,10 @@
+//console.log(document);
+//console.dir(document);
+//console.log(document.title);
+//console.log(document.URL);
+//document.title = "Title goes here!";
+//document.location = "http://www.google.com";
+
+document.body.style.backgroundColor = "pink";
+document.getElementById("myDiv").innerHTML = " Assalamu alaikum";
+
